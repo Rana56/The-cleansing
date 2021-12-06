dfdd64e1caec49b43e45fa61bd76e1ae486cbd78
Chirag <54218819+Rana56@users.noreply.github.com>
2021-12-06 00:44:22 +0000

    New commit

---
+++ TheCleansing(current)/Assets/Scripts/Combat/BattleSystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheCleansing.Combat
+{
+    public class BattleSystem : MonoBehaviour
+    {
+        //Start is called before the first frame update
+        public void Start()
+        {
+            
+        }
+    }
+}
---
+++ TheCleansing(current)/Assets/Scripts/Combat/Unit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheCleansing.Combat
+{
+    public class Unit : MonoBehaviour
+    {
+        public string unitName;                 //script stores basic info of characters
+        //public int unitLevel;
+
+        public int damage;
+
+        public int maxHP;
+        public int currentHP;
+    }
+}
