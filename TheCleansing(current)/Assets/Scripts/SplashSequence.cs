08d30758bdf205e0f638e0a4f42f99b1cadc8e04
Chirag <54218819+Rana56@users.noreply.github.com>
2022-02-12 23:21:18 +0000

    Update 1.8.3

--- TheCleansing(current)/Assets/Scripts/SplashSequence.cs
+++ TheCleansing(current)/Assets/Scripts/SplashSequence.cs
@@ -15,7 +15,7 @@ public class SplashSequence : MonoBehaviour
 
     IEnumerator ToMainMenu()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(2);
         SceneNumber = 1;
         SceneManager.LoadScene(1);
     }
