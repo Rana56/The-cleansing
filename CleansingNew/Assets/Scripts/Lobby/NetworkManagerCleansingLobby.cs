39abae54fa4fb0aa6f5aa9d76da306e16eb270b6
Chirag <54218819+Rana56@users.noreply.github.com>
2022-02-14 00:18:38 +0000

    First commit

---
+++ CleansingNew/Assets/Scripts/Lobby/NetworkManagerCleansingLobby.cs
@@ -0,0 +1,177 @@
+using Mirror;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CleansingNew.Lobby
+{
+    public class NetworkManagerCleansingLobby : NetworkManager
+    {
+        [SerializeField] private int minPlayers = 2;                        //minimum players needed to start game
+        [Scene] [SerializeField] private string menuScene = string.Empty;       //scene reference
+
+        [Header("Room")]
+        [SerializeField] private NetworkRoomPlayerLobby roomPlayerPrefab = null;             //reference to network room player
+                                                                                             //[SerializeField] string menuScene;
+
+        [Header("Game")]
+        //[SerializeField] private NetworkGamePlayerLobby gamePlayerPrefab = null;
+        [SerializeField] private GameObject playerSpawnSystem = null;                   //gameobject with the player spawn system
+
+        public static event Action OnClientConnected;
+        public static event Action OnClientDisconnected;
+        public static event Action<NetworkConnection> OnServerReadied;          //used to know if everyone has connected to the game and is ready to start on the server, include a timeout if someone disconnects
+
+        public List<NetworkRoomPlayerLobby> RoomPlayers { get; } = new List<NetworkRoomPlayerLobby>();          //stores all the joined player in a list, so they can all be accessed for functions
+        //public List<NetworkGamePlayerLobby> GamePlayers { get; } = new List<NetworkGamePlayerLobby>();          //stores all the players in the game
+
+        //loads all game objects from resources, under the spawnable prefabs. spawnable prefabs are objects the will spawn on the network
+        public override void OnStartServer() => spawnPrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs").ToList();
+
+        public override void OnStartClient()
+        {
+            var spawnablePrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs");
+
+            foreach (var prefab in spawnablePrefabs)
+            {
+                NetworkClient.RegisterPrefab(prefab);
+            }
+        }
+
+        public override void OnClientConnect(NetworkConnection conn)                //does base logic and raises the event
+        {
+            base.OnClientConnect(conn);
+
+            OnClientConnected?.Invoke();
+        }
+
+        public override void OnClientDisconnect(NetworkConnection conn)
+        {
+            base.OnClientDisconnect(conn);
+
+            OnClientDisconnected?.Invoke();
+        }
+
+        public override void OnServerConnect(NetworkConnection conn)
+        {
+            if (numPlayers >= maxConnections)                       //doesn't let player join if max players reached
+            {
+                conn.Disconnect();
+                return;
+            }
+
+            if (SceneManager.GetActiveScene().path != menuScene)            //doesn't let player join if game already started
+            {
+                conn.Disconnect();
+                return;
+            }
+        }
+
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            if (SceneManager.GetActiveScene().path == menuScene)
+            {
+                bool isLeader = RoomPlayers.Count == 0;           //first person in the lobby is assgined the leader
+
+                NetworkRoomPlayerLobby roomPlayerInstance = Instantiate(roomPlayerPrefab);              //spawns prefab
+
+                roomPlayerInstance.IsLeader = isLeader;             //sets person as leader and so will get leader privalages
+
+                NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);              //adds player for connection, assigns network connection to player
+            }
+        }
+
+        public override void OnServerDisconnect(NetworkConnection conn)
+        {
+            if (conn.identity != null)
+            {
+                var player = conn.identity.GetComponent<NetworkRoomPlayerLobby>();              //gets players room player script and removes it from the list on the server side
+
+                RoomPlayers.Remove(player);
+
+                NotifyPlayersOfReadyState();               //notifies other players on the client side
+            }
+
+            base.OnServerDisconnect(conn);          //destorys the player for the connection
+        }
+
+        public void NotifyPlayersOfReadyState()
+        {
+            foreach (var player in RoomPlayers)
+            {
+                player.HandleReadyToStart(IsReadyToStart());                //loops through all the players to see if they are ready to start
+            }
+        }
+
+        public bool IsReadyToStart()
+        {
+            if (numPlayers < minPlayers) { return false; }          //if min players not reached, game won't start
+
+            foreach (var player in RoomPlayers)                 //if all the players are not ready, game won't start
+            {
+                if (!player.IsReady)
+                {
+                    return false;
+                }
+            }
+
+            return true;        //returns true, if it doens't meet the previous if requirements
+        }
+
+        public override void OnStopServer()         //called when server is stopped, called for every client
+        {
+            RoomPlayers.Clear();
+        }
+        /**
+        public void StartGame()                     //when start button is pressed, this functio is run
+        {
+            if (SceneManager.GetActiveScene().path == menuScene)         //checks is current scene is the menu
+            {
+                if (!IsReadyToStart()) { return; }                  //checks if everyone is ready
+
+                ServerChangeScene("Main Game");                 //changes scene
+            }
+        }
+
+        public override void ServerChangeScene(string newSceneName)                 //method handles scene change, e.g. going to a game, going out of game
+        {
+            // From menu to game
+            if (SceneManager.GetActiveScene().path == menuScene && newSceneName.StartsWith("Main Game"))            //checks if current scene is the menu and if the new scene matches the string - going from the main menu to the game
+            {
+                for (int i = RoomPlayers.Count - 1; i >= 0; i--)                    //when going from menu to game, goes trough all the room players
+                {
+                    var conn = RoomPlayers[i].connectionToClient;                   //gets their connection
+                    var gameplayerInstance = Instantiate(gamePlayerPrefab);             //spawns in their game version of the prefab
+                    gameplayerInstance.SetDisplayName(RoomPlayers[i].DisplayName);      //sets the display name, transfers the name from the room to the game player
+
+                    NetworkServer.Destroy(conn.identity.gameObject);        //destorys their game object for thier current identity, i.e. gets rid of their room player object
+
+                    //the connection to the client is now not the object that was destroyed, it is the object that was spawned in
+                    NetworkServer.ReplacePlayerForConnection(conn, gameplayerInstance.gameObject);          //assigns the new game player to connection player insted of the old one
+                }
+            }
+
+            base.ServerChangeScene(newSceneName);       //does the base logic for chaging the scene
+        }**/
+
+        public override void OnServerReady(NetworkConnection conn)      //listens into whether the client is ready on the server
+        {
+            base.OnServerReady(conn);
+
+            OnServerReadied?.Invoke(conn);              //onserverReadied event raised
+        }
+
+        public override void OnServerChangeScene(string newSceneName)               //called on the server, when a scene is completed
+        {                                                                           //when the scene is loaded, actions can start on the scene
+            if (newSceneName.StartsWith("Main Game"))
+            {                           //checks if it is one of the levels   
+                GameObject playerSpawnSystemInstance = Instantiate(playerSpawnSystem);              //spwans the player spawn system
+                NetworkServer.Spawn(playerSpawnSystemInstance);                 //connection not passed as parameter, so the server owns it
+            }                                                                   //all clients has a spawn system and is owned by the server
+        }
+    }
+}
+
+
---
+++ CleansingNew/Assets/Scripts/Lobby/NetworkRoomPlayerLobby.cs
@@ -0,0 +1,128 @@
+﻿using Mirror;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CleansingNew.Lobby                   //a room player stores the user's name and if they are ready or not
+{
+    public class NetworkRoomPlayerLobby : NetworkBehaviour              //script sits on the player when they join and destroyed when they leave, each player has this script
+    {
+        [Header("UI")]
+        [SerializeField] private GameObject lobbyUI = null;             //turns the lobby on or off for the player
+        [SerializeField] private TMP_Text[] playerNameTexts = new TMP_Text[4];          //used for show player name
+        [SerializeField] private TMP_Text[] playerReadyTexts = new TMP_Text[4];         //used to show if players are ready to start
+        [SerializeField] private Button startGameButton = null;                 //shows this button only to the leader, allows them to start game when everyone is ready
+
+        [SyncVar(hook = nameof(HandleDisplayNameChanged))]              //variables that can only be changed on the server and is updated everywhere once updated
+        public string DisplayName = "Loading...";                   //server changes the names and the logic, it notifies all the other cilents
+        [SyncVar(hook = nameof(HandleReadyStatusChanged))]          //hook is the name of the method that is called when function is executed, e.g. when ready is called, this method is called, updated the ui only when the variables are changed
+        public bool IsReady = false;
+
+        private bool isLeader;
+        public bool IsLeader                //sets the leader boolean
+        {
+            set
+            {
+                isLeader = value;
+                startGameButton.gameObject.SetActive(value);            //activates the buttonif the leader is true
+            }
+        }
+
+        private NetworkManagerCleansingLobby room;
+        private NetworkManagerCleansingLobby Room        //a way to reference room easliy
+        {
+            get
+            {
+                if (room != null) { return room; }
+                return room = NetworkManager.singleton as NetworkManagerCleansingLobby;          //casts the networkManager as a networkManagerLobby
+            }
+        }
+
+        public override void OnStartAuthority()         //called on the object that belongs to the user
+        {
+            CmdSetDisplayName(PlayerNameInput.DisplayName);         //a function called by a client that runs on a server, gets player name from the player's input and sets it on the server and is validated
+
+            lobbyUI.SetActive(true);            //activates ui because its ourselves and not others
+        }
+
+        public override void OnStartClient()            //called on every network behaviour when active on a client
+        {
+            Room.RoomPlayers.Add(this);             //adds to list for room players, this instance added to list
+
+            UpdateDisplay();            //ui updated, e.g. whenever something starts or something is destoryed
+        }
+
+        public override void OnStopClient()             //called whenever anyone disconnects
+        {
+            Room.RoomPlayers.Remove(this);          //removes whoever disconnected
+
+            UpdateDisplay();
+        }
+
+        public void HandleReadyStatusChanged(bool oldValue, bool newValue) => UpdateDisplay();
+        public void HandleDisplayNameChanged(string oldValue, string newValue) => UpdateDisplay();
+
+        private void UpdateDisplay()
+        {
+            if (!hasAuthority)                  //looks for the player that has authority and updates their display
+            {
+                foreach (var player in Room.RoomPlayers)
+                {
+                    if (player.hasAuthority)
+                    {
+                        player.UpdateDisplay();
+                        break;
+                    }
+                }
+
+                return;
+            }
+
+            //better opitmised code would be to check which values need to be updated and only update the specified text, rather than clearing everying and setting values again
+
+            for (int i = 0; i < playerNameTexts.Length; i++)                //if player object belongs to us, goes through the name text and sets string
+            {
+                playerNameTexts[i].text = "Waiting For Player...";          //clears all the text and sets to empty
+                playerReadyTexts[i].text = string.Empty;
+            }
+
+            for (int i = 0; i < Room.RoomPlayers.Count; i++)
+            {
+                playerNameTexts[i].text = Room.RoomPlayers[i].DisplayName;          //sets player name to ui
+                playerReadyTexts[i].text = Room.RoomPlayers[i].IsReady ?            //sets player if they are ready or not
+                    "<color=green>Ready</color>" :                              //changes color depending if they are ready
+                    "<color=red>Not Ready</color>";
+            }
+        }
+
+        public void HandleReadyToStart(bool readyToStart)              //updates ready status
+        {
+            if (!isLeader) { return; }          //method only applicable to leader
+
+            startGameButton.interactable = readyToStart;            //button only interacatble if all players are ready
+        }
+
+        [Command]
+        private void CmdSetDisplayName(string displayName)              //when name recived by server, sets name of player
+        {
+            DisplayName = displayName;
+        }
+
+        [Command]
+        public void CmdReadyUp()                            //command run on server
+        {
+            IsReady = !IsReady;                             //server toggles if players have ready'd up 
+
+            Room.NotifyPlayersOfReadyState();               //notifies room status of player's ready up
+        }
+        /**
+        [Command]
+        public void CmdStartGame()
+        {
+            if (Room.RoomPlayers[0].connectionToClient != connectionToClient) { return; }               //checks if the first person in the room is the leader
+
+            Room.StartGame();
+        }**/
+    }
+}
