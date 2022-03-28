c10a138f1231615556c73ab99e3c57be5c74de8d
Kyushu <abdulalian201@gmail.com>
2022-03-28 20:54:07 +0100

    Spawning in Player Models.
    made some changes where it allowed to choose classes rather than stuck at one

--- CleansingNew/Assets/Scripts/Lobby/PlayerSpawnSystem.cs
+++ CleansingNew/Assets/Scripts/Lobby/PlayerSpawnSystem.cs
@@ -7,11 +7,23 @@ namespace TheCleansing.Lobby
 {
     public class PlayerSpawnSystem : NetworkBehaviour                   //spawn system created by the server, when spawning to a scene a game object with this scripte will be spawned
     {
-        [SerializeField] private GameObject playerPrefab = null;        //the prefab that will spawn, assigned to a connection
+        [SerializeField] private GameObject TankPrefab = null; 
+        [SerializeField] private GameObject SoliderPrefab = null;
+        [SerializeField] private GameObject HealerPrefab = null;
+        private GameObject playerPrefab = null;        //the prefab that will spawn, assigned to a connection
         private static List<Transform> spawnPoints = new List<Transform>();         //stores the posisitions in the scene, transform - position, rotation, scale
 
         private int nextIndex = 0;          //when player spawns in, this allows the server to know where to spawn next player
 
+        private NetworkManagerTC game;
+        private NetworkManagerTC Game        //a way to reference game easliy
+        {
+            get
+            {
+                if (game != null) { return game; }
+                return game = NetworkManager.singleton as NetworkManagerTC;          //casts the networkManager as a networkManagerLobby
+            }
+        }
         public static void AddSpawnPoint(Transform transform)               //spawns system doesn't exist during desgin time, spwaned later, method called by other objects
         {
             spawnPoints.Add(transform);             //adds to the list
@@ -19,7 +31,37 @@ namespace TheCleansing.Lobby
             spawnPoints = spawnPoints.OrderBy(x => x.GetSiblingIndex()).ToList();           //makes sure the order is correct
         }
 
+        string getLocalPlayerClass()
+        {
+            TheCleansing.Lobby.NetworkGamePlayer SelectedPlayer = null;
+            string  SelectedClass = null;
+            for (int i = 0; i < Game.GamePlayers.Count; i++)
+            {
+                if (Game.GamePlayers[i].name.Equals("LocalGamePlayer"))
+                {
+                    SelectedPlayer = Game.GamePlayers[i];
+                    break;
+                }
 
+            }
+            SelectedClass = SelectedPlayer.CharacterClass;
+
+            return SelectedClass;
+        }
+            
+         void ChooseClass(string Class)
+        {
+            if(Class == "Tank")
+            {
+                playerPrefab = TankPrefab;
+            }else if(Class == "Solider")
+            {
+                playerPrefab= SoliderPrefab;
+            }else if(Class == "Medic")
+            {
+                playerPrefab = HealerPrefab;
+            }
+        }
         public static void RemoveSpawnPoint(Transform transform) => spawnPoints.Remove(transform);          //removes spawn point
 
         public override void OnStartServer() => NetworkManagerTC.OnServerReadied += SpawnPlayer;         //when this game object starts existing on the server, the object is subscribed to the onserverReadied event
@@ -37,7 +79,7 @@ namespace TheCleansing.Lobby
                 Debug.LogError($"Missing spawn point for player {nextIndex}");
                 return;
             }
-
+            ChooseClass(getLocalPlayerClass());
             GameObject playerInstance = Instantiate(playerPrefab, spawnPoints[nextIndex].position, spawnPoints[nextIndex].rotation);           //spawns in the player, instantiates the prefab, spawns it at the position from spawnPoints facing a certian direction (rotation)
             
             NetworkServer.Spawn(playerInstance, conn);          //spwans it for the other clients, connection also passed a parameter to show the connection belongs to the player object that is spawned in - the user has authority over it
